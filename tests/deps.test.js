2866890f07abb34ec4a79aaa88925f49a8ade23d
Sergei Nikitin <nik.pasaran@gmail.com>
2019-03-28 15:07:31 +0300

    Еще правки по депсам и тесты

--- tests/deps.test.js
+++ tests/deps.test.js
@@ -1,6 +1,7 @@
 const de = require( '../lib' );
 
 const {
+    get_timeout,
     //  wait_for_value,
     //  wait_for_error,
     get_result_block,
@@ -11,17 +12,163 @@ const {
 
 describe( 'options.deps', () => {
 
-    test( 'failed deps #1', async () => {
+    test( 'block depends on block #1 (deps is id)', async () => {
+        const spy = jest.fn();
+
+        const block_foo = get_result_block( () => spy( 'FOO' ), 50 );
+        const block_bar = get_result_block( () => spy( 'BAR' ), 50 );
+
         const id_foo = Symbol( 'foo' );
+
+        const block = de.object( {
+            block: {
+                foo: block_foo( {
+                    options: {
+                        id: id_foo,
+                    },
+                } ),
+                bar: block_bar( {
+                    options: {
+                        deps: id_foo,
+                    },
+                } ),
+            },
+        } );
+
+        const context = new de.Context();
+        await context.run( block );
+
+        const calls = spy.mock.calls;
+
+        expect( calls.length ).toBe( 2 );
+        expect( calls[ 0 ][ 0 ] ).toBe( 'FOO' );
+        expect( calls[ 1 ][ 0 ] ).toBe( 'BAR' );
+    } );
+
+    test( 'block depends on block #2 (deps is array)', async () => {
+        const spy = jest.fn();
+
+        const block_foo = get_result_block( () => spy( 'FOO' ), 50 );
+        const block_bar = get_result_block( () => spy( 'BAR' ), 50 );
+
+        const id_foo = Symbol( 'foo' );
+
+        const block = de.object( {
+            block: {
+                foo: block_foo( {
+                    options: {
+                        id: id_foo,
+                    },
+                } ),
+                bar: block_bar( {
+                    options: {
+                        deps: [ id_foo ],
+                    },
+                } ),
+            },
+        } );
+
+        const context = new de.Context();
+        await context.run( block );
+
+        const calls = spy.mock.calls;
+
+        expect( calls.length ).toBe( 2 );
+        expect( calls[ 0 ][ 0 ] ).toBe( 'FOO' );
+        expect( calls[ 1 ][ 0 ] ).toBe( 'BAR' );
+    } );
+
+    test( 'block depends on block depends on block', async () => {
+        const spy = jest.fn();
+
+        const block_foo = get_result_block( () => spy( 'FOO' ), 50 );
+        const block_bar = get_result_block( () => spy( 'BAR' ), 50 );
+        const block_quu = get_result_block( () => spy( 'QUU' ), 50 );
+
+        const id_foo = Symbol( 'foo' );
+        const id_bar = Symbol( 'bar' );
+
+        const block = de.object( {
+            block: {
+                foo: block_foo( {
+                    options: {
+                        id: id_foo,
+                    },
+                } ),
+                bar: block_bar( {
+                    options: {
+                        id: id_bar,
+                        deps: id_foo,
+                    },
+                } ),
+                quu: block_quu( {
+                    options: {
+                        deps: id_bar,
+                    },
+                } ),
+            },
+        } );
+
+        const context = new de.Context();
+        await context.run( block );
+
+        const calls = spy.mock.calls;
+
+        expect( calls.length ).toBe( 3 );
+        expect( calls[ 0 ][ 0 ] ).toBe( 'FOO' );
+        expect( calls[ 1 ][ 0 ] ).toBe( 'BAR' );
+        expect( calls[ 2 ][ 0 ] ).toBe( 'QUU' );
+    } );
+
+    test( 'block depends on two blocks', async () => {
+        const spy = jest.fn();
+
+        const block_foo = get_result_block( () => spy( 'FOO' ), get_timeout( 50, 100 ) );
+        const block_bar = get_result_block( () => spy( 'BAR' ), get_timeout( 50, 100 ) );
+        const block_quu = get_result_block( () => spy( 'QUU' ), 50 );
+
+        const id_foo = Symbol( 'foo' );
+        const id_bar = Symbol( 'bar' );
+
+        const block = de.object( {
+            block: {
+                foo: block_foo( {
+                    options: {
+                        id: id_foo,
+                    },
+                } ),
+                bar: block_bar( {
+                    options: {
+                        id: id_bar,
+                    },
+                } ),
+                quu: block_quu( {
+                    options: {
+                        deps: [ id_foo, id_bar ],
+                    },
+                } ),
+            },
+        } );
+
+        const context = new de.Context();
+        await context.run( block );
+
+        const calls = spy.mock.calls;
+
+        expect( calls.length ).toBe( 3 );
+        expect( calls[ 2 ][ 0 ] ).toBe( 'QUU' );
+    } );
+
+    test( 'failed deps #1', async () => {
         const error_foo = de.error( {
             id: 'SOME_ERROR',
         } );
-        const block_foo = get_error_block( error_foo, 300 );
+        const block_foo = get_error_block( error_foo, 50 );
 
         const body_bar = jest.fn();
-        const block_bar = de.func( {
-            block: body_bar,
-        } );
+        const block_bar = get_result_block( body_bar, 50 );
+
+        const id_foo = Symbol( 'foo' );
 
         const block = de.object( {
             block: {
@@ -49,22 +196,21 @@ describe( 'options.deps', () => {
     } );
 
     test( 'failed deps #2', async () => {
-        const id_foo = Symbol( 'foo' );
         const error_foo = de.error( {
             id: 'SOME_ERROR_1',
         } );
-        const block_foo = get_error_block( error_foo, 300 );
+        const block_foo = get_error_block( error_foo, 100 );
 
-        const id_bar = Symbol( 'bar' );
         const error_bar = de.error( {
             id: 'SOME_ERROR_2',
         } );
-        const block_bar = get_error_block( error_bar, 100 );
+        const block_bar = get_error_block( error_bar, 50 );
 
         const body_quu = jest.fn();
-        const block_quu = de.func( {
-            block: body_quu,
-        } );
+        const block_quu = get_result_block( body_quu, 50 );
+
+        const id_foo = Symbol( 'foo' );
+        const id_bar = Symbol( 'bar' );
 
         const block = de.object( {
             block: {
@@ -93,20 +239,20 @@ describe( 'options.deps', () => {
 
         expect( de.is_error( result.quu ) ).toBe( true );
         expect( result.quu.error.id ).toBe( de.ERROR_ID.DEPS_ERROR );
-        //  block_bar падает за 100 мс, а block_foo за 300 мс.
+        //  block_bar падает за 50 мс, а block_foo за 100 мс.
         //  Поэтому в reason будет error_bar.
         expect( result.quu.error.reason ).toBe( error_bar );
         expect( body_quu ).toHaveBeenCalledTimes( 0 );
     } );
 
     test( 'before( { deps } ) has deps results #1', async () => {
-        const id_foo = Symbol( 'foo' );
         const data_foo = {
             foo: 42,
         };
-        const block_foo = get_result_block( data_foo, 300 );
+        const block_foo = get_result_block( data_foo, 50 );
+        const block_bar = get_result_block( null, 50 );
 
-        const block_bar = get_result_block( null, 200 );
+        const id_foo = Symbol( 'foo' );
         const before_bar = jest.fn();
 
         const block = de.object( {
@@ -135,20 +281,21 @@ describe( 'options.deps', () => {
     } );
 
     test( 'before( { deps } ) has deps results #2', async () => {
-        const id_foo = Symbol( 'foo' );
         const data_foo = {
             foo: 42,
         };
         const block_foo = get_result_block( data_foo, 300 );
 
-        const id_bar = Symbol( 'bar' );
         const data_bar = {
             bar: 24,
         };
         const block_bar = get_result_block( data_bar, 200 );
 
-        const before_quu = jest.fn();
         const block_quu = get_result_block( null, 100 );
+
+        const id_foo = Symbol( 'foo' );
+        const id_bar = Symbol( 'bar' );
+        const before_quu = jest.fn();
 
         const block = de.object( {
             block: {
@@ -182,21 +329,70 @@ describe( 'options.deps', () => {
         expect( deps[ id_bar ] ).toBe( data_bar );
     } );
 
+    test( 'before( { deps } ) has not results from other blocks', async () => {
+        const data_foo = {
+            foo: 42,
+        };
+        const block_foo = get_result_block( data_foo, 50 );
+
+        const data_bar = {
+            bar: 24,
+        };
+        const block_bar = get_result_block( data_bar, 100 );
+
+        const block_quu = get_result_block( null, 50 );
+
+        const id_foo = Symbol( 'foo' );
+        const id_bar = Symbol( 'bar' );
+        const before_quu = jest.fn();
+
+        const block = de.object( {
+            block: {
+                foo: block_foo( {
+                    options: {
+                        id: id_foo,
+                    },
+                } ),
+
+                bar: block_bar( {
+                    options: {
+                        id: id_bar,
+                    },
+                } ),
+
+                quu: block_quu( {
+                    options: {
+                        deps: id_bar,
+
+                        before: before_quu,
+                    },
+                } ),
+            },
+        } );
+
+        const context = new de.Context();
+        await context.run( block );
+
+        const deps = before_quu.mock.calls[ 0 ][ 0 ].deps;
+        expect( deps[ id_foo ] ).toBe( undefined );
+        expect( deps[ id_bar ] ).toBe( data_bar );
+    } );
+
     it( 'wait for result of de.func', async () => {
         const id_foo = Symbol( 'foo' );
         const data_foo = {
             foo: 42,
         };
-        const block_foo = get_result_block( data_foo, 300 )( {
+        const block_foo = get_result_block( data_foo, 50 )( {
             options: {
                 id: id_foo,
             },
         } );
 
-        const block_bar = get_result_block( block_foo, 200 );
+        const block_bar = get_result_block( block_foo, 50 );
 
         const before_quu = jest.fn();
-        const block_quu = get_result_block( null );
+        const block_quu = get_result_block( null, 50 );
 
         const block = de.object( {
             block: {
@@ -218,6 +414,190 @@ describe( 'options.deps', () => {
 
         const deps = before_quu.mock.calls[ 0 ][ 0 ].deps;
         expect( deps[ id_foo ] ).toBe( data_foo );
+    } );
+
+    it( 'unresolved deps #1', async () => {
+        const id_foo = Symbol( 'foo' );
+
+        const block = get_result_block( null, 50 )( {
+            options: {
+                deps: id_foo,
+            },
+        } );
+
+        expect.assertions( 2 );
+
+        const context = new de.Context();
+        try {
+            await context.run( block );
+
+        } catch ( error ) {
+            expect( de.is_error( error ) ).toBe( true );
+            expect( error.error.id ).toBe( de.ERROR_ID.DEPS_NOT_RESOLVED );
+        }
+    } );
+
+    it( 'unresolved deps #2', async () => {
+        const block_foo = get_result_block( null, 50 );
+        const block_bar = get_result_block( null, 50 );
+
+        const foo_id = Symbol( 'foo' );
+
+        const block = de.object( {
+            block: {
+                foo: block_foo,
+
+                bar: block_bar( {
+                    options: {
+                        deps: foo_id,
+                    },
+                } ),
+            },
+        } );
+
+        const context = new de.Context();
+        const result = await context.run( block );
+
+        expect( de.is_error( result.bar ) ).toBe( true );
+        expect( result.bar.error.id ).toBe( de.ERROR_ID.DEPS_NOT_RESOLVED );
+    } );
+
+    it( 'preconditions #1', async () => {
+        const state = {};
+
+        const data_foo = {
+            foo: 42,
+        };
+        const block_foo = get_result_block( data_foo, 50 );
+        const block_bar = get_result_block( () => state.foo, 50 );
+
+        const block = de.object( {
+            block: {
+                foo: block_foo( {
+                    options: {
+                        after: function( { result } ) {
+                            state.foo = result;
+                        },
+                    },
+                } ),
+
+                bar: block_bar( {
+                    options: {
+                        deps: [
+                            () => Boolean( state.foo ),
+                        ],
+                    },
+                } ),
+            },
+        } );
+
+        const context = new de.Context();
+        const result = await context.run( block );
+
+        expect( result.bar ).toBe( data_foo );
+    } );
+
+    it( 'preconditions #2', async () => {
+        const state = {};
+
+        const data_foo = {
+            foo: 42,
+        };
+        const block_foo = get_result_block( data_foo, get_timeout( 50, 100 ) );
+
+        const data_bar = {
+            bar: 24,
+        };
+        const block_bar = get_result_block( data_bar, get_timeout( 50, 100 ) );
+
+        const block_quu = get_result_block( () => {
+            return {
+                foo: state.foo,
+                bar: state.bar,
+            };
+        }, 50 );
+
+        const block = de.object( {
+            block: {
+                foo: block_foo( {
+                    options: {
+                        after: function( { result } ) {
+                            state.foo = result;
+                        },
+                    },
+                } ),
+
+                bar: block_bar( {
+                    options: {
+                        after: function( { result } ) {
+                            state.bar = result;
+                        },
+                    },
+                } ),
+
+                quu: block_quu( {
+                    options: {
+                        deps: [
+                            () => Boolean( state.foo ),
+                            () => Boolean( state.bar ),
+                        ],
+                    },
+                } ),
+            },
+        } );
+
+        const context = new de.Context();
+        const result = await context.run( block );
+
+        expect( result.quu ).toEqual( {
+            foo: data_foo,
+            bar: data_bar,
+        } );
+        expect( result.quu.foo ).toBe( data_foo );
+        expect( result.quu.bar ).toBe( data_bar );
+    } );
+
+    it( 'preconditions #3', async () => {
+        const data_foo = {
+            foo: 42,
+        };
+        const block_foo = get_result_block( null, 50 );
+        const block_bar = get_result_block( data_foo, 100 );
+
+        const state = {};
+
+        const block_quu = get_result_block( () => state.foo, 50 );
+
+        const block = de.object( {
+            block: {
+                foo: block_foo( {
+                    options: {
+                        after: function() {
+                            state.foo = null;
+                        },
+                    },
+                } ),
+
+                bar: block_bar( {
+                    options: {
+                        after: function( { result } ) {
+                            state.foo = result;
+                        },
+                    },
+                } ),
+
+                quu: block_quu( {
+                    options: {
+                        deps: () => Boolean( state.foo ),
+                    },
+                } ),
+            },
+        } );
+
+        const context = new de.Context();
+        const result = await context.run( block );
+
+        expect( result.quu ).toBe( data_foo );
     } );
 
 } );
--- tests/helpers.js
+++ tests/helpers.js
@@ -27,6 +27,16 @@ function wait_for_error( error, timeout ) {
 }
 
 function get_result_block( value, timeout ) {
+    if ( !de.is_block( value ) && ( typeof value === 'function' ) ) {
+        return de.func( {
+            block: async function() {
+                await wait_for_value( null, timeout );
+
+                return value();
+            },
+        } );
+    }
+
     return de.func( {
         block: function() {
             return wait_for_value( value, timeout );
@@ -35,6 +45,16 @@ function get_result_block( value, timeout ) {
 }
 
 function get_error_block( error, timeout ) {
+    if ( !de.is_block( error ) && ( typeof value === 'function' ) ) {
+        return de.func( {
+            block: async function() {
+                await wait_for_value( null, timeout );
+
+                throw error();
+            },
+        } );
+    }
+
     return de.func( {
         block: function() {
             return wait_for_error( error, timeout );
@@ -42,9 +62,14 @@ function get_error_block( error, timeout ) {
     } );
 }
 
+function get_timeout( from, to ) {
+    return Math.round( from + ( Math.random() * ( to - from ) ) );
+}
+
 //  ---------------------------------------------------------------------------------------------------------------  //
 
 module.exports = {
+    get_timeout,
     wait_for_value,
     wait_for_error,
     get_result_block,
