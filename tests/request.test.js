de8a920766128bbf2bfa34e13552bb217e95ed49
Sergei Nikitin <nik.pasaran@gmail.com>
2019-04-18 12:36:15 +0300

    moar tests

--- tests/request.test.js
+++ tests/request.test.js
@@ -16,7 +16,15 @@ const DEFAULT_OPTIONS = {
 };
 
 function do_request( options, context, cancel ) {
+    context = context || new de.Context();
+    cancel = cancel || new de.Cancel();
+
     return request( { ...DEFAULT_OPTIONS, ...options }, context, cancel );
+}
+
+let _path_index = 1;
+function get_path() {
+    return `/test/${ _path_index++ }`;
 }
 
 //  ---------------------------------------------------------------------------------------------------------------  //
@@ -27,10 +35,8 @@ describe( 'request', () => {
     beforeAll( () => fake.start() );
     afterAll( () => fake.stop() );
 
-    let n = 1;
-
     it( 'get', async () => {
-        const path = `/get/${ n++ }/`;
+        const path = get_path();
 
         const CONTENT = 'Hello';
 
@@ -39,16 +45,35 @@ describe( 'request', () => {
             content: CONTENT,
         } );
 
-        const context = new de.Context();
-        const cancel = new de.Cancel();
         const result = await do_request( {
             path: path,
-        }, context, cancel );
+        } );
 
         expect( result.status_code ).toBe( 200 );
         expect( Buffer.isBuffer( result.body ) ).toBe( true );
         expect( result.body.toString() ).toBe( CONTENT );
     } );
 
+    it( 'invalid protocol', async () => {
+        const path = get_path();
+
+        const CONTENT = 'Hello';
+
+        fake.add( path, {
+            status_code: 200,
+            content: CONTENT,
+        } );
+
+        expect.assertions( 1 );
+        try {
+            await do_request( {
+                protocol: 'http',
+                path: path,
+            } );
+
+        } catch ( error ) {
+            expect( de.is_error( error ) ).toBe( true );
+        }
+    } );
 } );
 
