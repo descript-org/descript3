3b2cf7f3fc335dbbe63ca40f8c92b945fb50bd18
Sergei Nikitin <nik.pasaran@gmail.com>
2020-07-21 18:25:23 +0300

    In progress

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,11 @@
+export type First< T > =
+    T extends readonly [ infer First, ...infer Rest ] | [ infer First, ...infer Rest ] ? First : never;
+
+export type Tail< T > =
+    T extends readonly [ infer First, ...infer Rest ] | [ infer First, ...infer Rest ] ? Rest : never;
+
+//  ---------------------------------------------------------------------------------------------------------------  //
+
 import {
     RequestOptions as HttpsRequestOptions,
     Agent as HttpsAgent,
@@ -32,9 +40,9 @@ declare class Cancel {
 
 //  ---------------------------------------------------------------------------------------------------------------  //
 
-declare class Cache {
-    get( args: { key: string, context: DescriptContext } ): DescriptBlockResult | Promise< DescriptBlockResult >;
-    set( args: { key: string, value: DescriptBlockResult, maxage: number, context: DescriptContext } ): void;
+declare class Cache< Result, Context > {
+    get( args: { key: string, context: Context } ): Result | Promise< Result >;
+    set( args: { key: string, value: Result, maxage: number, context: Context } ): void;
 }
 
 //  ---------------------------------------------------------------------------------------------------------------  //
@@ -75,8 +83,8 @@ interface LoggerEvent {
     error?: DescriptError;
 }
 
-interface DescriptLogger {
-    log( event: LoggerEvent, context: DescriptContext ): void;
+interface DescriptLogger< Context > {
+    log( event: LoggerEvent, context: Context ): void;
 }
 
 declare namespace Logger {
@@ -89,87 +97,98 @@ declare namespace Logger {
 
 //  ---------------------------------------------------------------------------------------------------------------  //
 
-type DescriptContext = any;
-
-//  ---------------------------------------------------------------------------------------------------------------  //
-
 type DescriptBlockId = symbol;
 type DescriptBlockGenerateId = () => DescriptBlockId;
 
-type DescriptBlockParams = Record< string, string | number | boolean | null | undefined >;
+type DescriptBlockDeps = Record< DescriptBlockId, any >;
 
-type DescriptBlockResult = DescriptJSON;
+/*
+type DescriptBlockResult<
+    Params,
+    Context,
+    Result,
+> =
+    Result |
+    Promise< Result >;
+    //  DescriptBlock< Params, Context, Result > |
+    //  Promise< DescriptBlockResult< Params, Context, Result > >;
+*/
 
-type DescriptBlockDeps = Record< DescriptBlockId, DescriptBlockResult >;
-
-type DescriptBlockBeforeResult = DescriptBlockResult | DescriptBlock | Promise< DescriptBlockBeforeResult >;
-type DescriptBlockAfterResult = DescriptBlockResult | DescriptBlock | Promise< DescriptBlockAfterResult >;
-
-interface DescriptBlockBaseCallbackArgs {
-    params: DescriptBlockParams;
-    context: DescriptContext;
+interface DescriptBlockBaseCallbackArgs<
+    Params,
+    Context,
+> {
+    params: Params;
+    context: Context;
     deps: DescriptBlockDeps;
 }
 
-interface DescriptBlockOptions {
+interface DescriptBlockOptions<
+    ParamsIn,
+    ParamsOut,
+    Context,
+    ResultIn,
+    ResultOut,
+> {
     name?: string;
 
     id?: DescriptBlockId;
     deps?: DescriptBlockId | Array< DescriptBlockId >;
 
-    params?: ( args: DescriptBlockBaseCallbackArgs ) => DescriptBlockParams;
+    params?: ( args: DescriptBlockBaseCallbackArgs< ParamsIn, Context > ) => ParamsOut;
 
-    before?: ( args: DescriptBlockBaseCallbackArgs & {
+    before?: ( args: DescriptBlockBaseCallbackArgs< ParamsOut, Context > & {
         cancel: Cancel,
-    } ) => DescriptBlockBeforeResult;
+    } ) => ResultOut | void;
 
-    after?: ( args: DescriptBlockBaseCallbackArgs & {
+    after?: ( args: DescriptBlockBaseCallbackArgs< ParamsOut, Context > & {
         cancel: Cancel,
-        result: DescriptBlockResult,
-    } ) => DescriptBlockBeforeResult;
+        result: ResultIn,
+    } ) => ResultOut;
 
-    error?: ( args: DescriptBlockBaseCallbackArgs & {
+    error?: ( args: DescriptBlockBaseCallbackArgs< ParamsOut, Context > & {
         cancel: Cancel,
         error: DescriptError,
-    } ) => DescriptBlockBeforeResult;
+    } ) => ResultOut | void;
 
     timeout?: number;
 
-    key?: string | ( ( args: DescriptBlockBaseCallbackArgs ) => string );
+    key?: string | ( ( args: DescriptBlockBaseCallbackArgs< ParamsOut, Context > ) => string );
     maxage?: number;
-    cache?: Cache;
+    cache?: Cache< ResultOut, Context >;
 
     required?: boolean;
 
-    logger?: DescriptLogger;
+    logger?: DescriptLogger< Context >;
 }
 
 //  ---------------------------------------------------------------------------------------------------------------  //
+//  HttpBlock
 
-type DescriptHttpBlockDescriptionCallback< T > = T | ( ( args: DescriptBlockBaseCallbackArgs ) => T );
+type DescriptHttpBlockDescriptionCallback< T, Params, Context > = T | ( ( args: DescriptBlockBaseCallbackArgs< Params, Context > ) => T );
 
 type DescriptHttpBlockQueryValue = string | number | boolean;
 type DescriptHttpBlockQuery = Record< string, DescriptHttpBlockQueryValue >;
 
 type DescriptHttpBlockHeaders = Record< string, string >;
 
-interface DescriptHttpBlockDescription {
-    protocol?: DescriptHttpBlockDescriptionCallback< string >;
-    hostname?: DescriptHttpBlockDescriptionCallback< string >;
-    port?: DescriptHttpBlockDescriptionCallback< number >;
-    method?: DescriptHttpBlockDescriptionCallback< string >;
-    pathname?: DescriptHttpBlockDescriptionCallback< string >;
+interface DescriptHttpBlockDescription< Params, Context > {
+    protocol?: DescriptHttpBlockDescriptionCallback< string, Params, Context >;
+    hostname?: DescriptHttpBlockDescriptionCallback< string, Params, Context >;
+    port?: DescriptHttpBlockDescriptionCallback< number, Params, Context >;
+    method?: DescriptHttpBlockDescriptionCallback< string, Params, Context >;
+    pathname?: DescriptHttpBlockDescriptionCallback< string, Params, Context >;
 
     query?:
         Record< string,
             DescriptHttpBlockQueryValue |
             null |
-            ( ( args: DescriptBlockBaseCallbackArgs & {
+            ( ( args: DescriptBlockBaseCallbackArgs< Params, Context > & {
                 query: DescriptHttpBlockQuery,
             } ) => DescriptHttpBlockQueryValue )
         > |
         (
-            ( args: DescriptBlockBaseCallbackArgs & {
+            ( args: DescriptBlockBaseCallbackArgs< Params, Context > & {
                 query: DescriptHttpBlockQuery,
             } ) => DescriptHttpBlockQuery
         );
@@ -177,12 +196,12 @@ interface DescriptHttpBlockDescription {
     headers?:
         Record< string,
             string |
-            ( ( args: DescriptBlockBaseCallbackArgs & {
+            ( ( args: DescriptBlockBaseCallbackArgs< Params, Context > & {
                 headers: DescriptHttpBlockHeaders,
             } ) => string )
         > |
         (
-            ( args: DescriptBlockBaseCallbackArgs & {
+            ( args: DescriptBlockBaseCallbackArgs< Params, Context > & {
                 headers: DescriptHttpBlockHeaders,
             } ) => DescriptHttpBlockHeaders
         );
@@ -190,7 +209,7 @@ interface DescriptHttpBlockDescription {
     body?:
         string |
         Buffer |
-        ( ( args: DescriptBlockBaseCallbackArgs ) => string | Buffer | DescriptJSON );
+        ( ( args: DescriptBlockBaseCallbackArgs< Params, Context > ) => string | Buffer | DescriptJSON );
 
     is_json?: boolean;
 
@@ -204,50 +223,140 @@ interface DescriptHttpBlockDescription {
 
     prepare_request_options?: ( options: HttpsRequestOptions ) => HttpsRequestOptions;
 
-    family?: DescriptHttpBlockDescriptionCallback< number >;
+    family?: DescriptHttpBlockDescriptionCallback< number, Params, Context >;
 
     agent?: HttpsAgent | HttpsAgentOptions | false;
 
-    auth?: DescriptHttpBlockDescriptionCallback< string >;
+    auth?: DescriptHttpBlockDescriptionCallback< string, Params, Context >;
 
-    ca?: DescriptHttpBlockDescriptionCallback< string | Buffer >;
-    cert?: DescriptHttpBlockDescriptionCallback< string | Buffer >;
-    ciphers?: DescriptHttpBlockDescriptionCallback< string >;
-    key?: DescriptHttpBlockDescriptionCallback< string | Buffer >;
-    passphrase?: DescriptHttpBlockDescriptionCallback< string >;
-    pfx?: DescriptHttpBlockDescriptionCallback< string | Buffer >;
-    rejectUnauthorized?: DescriptHttpBlockDescriptionCallback< boolean >;
-    secureProtocol?: DescriptHttpBlockDescriptionCallback< string >;
-    servername?: DescriptHttpBlockDescriptionCallback< string >;
+    ca?: DescriptHttpBlockDescriptionCallback< string | Buffer, Params, Context >;
+    cert?: DescriptHttpBlockDescriptionCallback< string | Buffer, Params, Context >;
+    ciphers?: DescriptHttpBlockDescriptionCallback< string, Params, Context >;
+    key?: DescriptHttpBlockDescriptionCallback< string | Buffer, Params, Context >;
+    passphrase?: DescriptHttpBlockDescriptionCallback< string, Params, Context >;
+    pfx?: DescriptHttpBlockDescriptionCallback< string | Buffer, Params, Context >;
+    rejectUnauthorized?: DescriptHttpBlockDescriptionCallback< boolean, Params, Context >;
+    secureProtocol?: DescriptHttpBlockDescriptionCallback< string, Params, Context >;
+    servername?: DescriptHttpBlockDescriptionCallback< string, Params, Context >;
 }
 
-interface DescriptHttpBlock {
-    ( args: { block?: DescriptHttpBlockDescription, options?: DescriptBlockOptions } ): DescriptHttpBlock;
+interface DescriptHttpBlock<
+    Params,
+    Context,
+    Result,
+> {
+    < ExtendedParamsIn, ExtendedResultOut >( args: {
+        block?: DescriptHttpBlockDescription< Params, Context >,
+        options?: DescriptBlockOptions< ExtendedParamsIn, Params, Context, Result, ExtendedResultOut >,
+    } ): DescriptHttpBlock< ExtendedParamsIn, Context, ExtendedResultOut >;
 }
 
+declare function http<
+    ParamsIn,
+    ParamsOut,
+    Context,
+    ResultIn,
+    ResultOut,
+> (
+    args: {
+        block: DescriptHttpBlockDescription< ParamsOut, Context >,
+        options?: DescriptBlockOptions< ParamsIn, ParamsOut, Context, ResultIn, ResultOut >,
+    },
+): DescriptHttpBlock< ParamsIn, Context, ResultOut >;
+
+
+/*
 //  ---------------------------------------------------------------------------------------------------------------  //
+//  FuncBlock
 
-type DescriptFuncBlockDescription = ( args: DescriptBlockBaseCallbackArgs & {
+type DescriptFuncBlockDescription<
+    Params,
+    Context,
+    Result,
+> = ( args: DescriptBlockBaseCallbackArgs< Params, Context > & {
     generate_id: DescriptBlockGenerateId,
     cancel: Cancel,
 } ) =>
-    DescriptBlock |
-    DescriptBlockResult |
-    Promise< DescriptBlock | DescriptBlockResult >;
+    Result |
+    //  DescriptBlock< Params, Context, Result > |
+    Promise< Result >;
 
-interface DescriptFuncBlock {
-    ( args: { block?: DescriptFuncBlockDescription, options?: DescriptBlockOptions } ): DescriptFuncBlock;
+interface DescriptFuncBlock<
+    Params,
+    Context,
+    Result,
+> {
+    < ExtendedParamsIn, ExtendedResultOut >( args: {
+        block?: DescriptFuncBlockDescription< Params, Context, Result >,
+        options?: DescriptBlockOptions<ExtendedParamsIn, Params, Context, Result, ExtendedResultOut >,
+    } ): DescriptFuncBlock< ExtendedParamsIn, Context, ExtendedResultOut >;
 }
 
+declare function func<
+    ParamsIn,
+    ParamsOut,
+    Context,
+    ResultIn,
+    ResultOut,
+> (
+    args: {
+        block: DescriptFuncBlockDescription< ParamsOut, Context, ResultOut >,
+        options?: DescriptBlockOptions< ParamsIn, ParamsOut, Context, ResultIn, ResultOut >,
+    },
+): DescriptFuncBlock< ParamsIn, Context, ResultOut >;
+*/
+
 //  ---------------------------------------------------------------------------------------------------------------  //
+//  ArrayBlock
 
-type DescriptArrayBlockDescription = Array< DescriptBlock >;
+export type GetArrayBlockResult< T > = {
+    0: never,
+    1: [ GetDescriptBlockResult< First< T > > ],
+    2: [ GetDescriptBlockResult< First< T > >,  ...GetArrayBlockResult< Tail< T > > ],
+}[ T extends [] ? 0 : T extends ( ( readonly [ any ] ) | [ any ] ) ? 1 : 2 ];
 
-interface DescriptArrayBlock {
-    ( args: { block?: DescriptArrayBlockDescription, options?: DescriptBlockOptions } ): DescriptArrayBlock;
+export type GetArrayBlockParams< T > = {
+    0: never,
+    1: GetDescriptBlockParams< First< T > >,
+    2: GetDescriptBlockParams< First< T > > & GetArrayBlockParams< Tail< T > >,
+}[ T extends [] ? 0 : T extends ( ( readonly [ any ] ) | [ any ] ) ? 1 : 2 ];
+
+type DescriptArrayBlockDescription< T extends ReadonlyArray< any > | Array< any > > = {
+    [ P in keyof T ]: T[ P ] extends DescriptBlock< infer Params, infer Context, infer Result > ? T[ P ] : never
 }
 
+interface DescriptArrayBlock<
+    Params,
+    Context,
+    Result,
+> {
+    < ExtendedParamsIn, ExtendedResultOut >( args: {
+        options?: DescriptBlockOptions< ExtendedParamsIn, Params, Context, Result, ExtendedResultOut >,
+    } ): DescriptArrayBlock< ExtendedParamsIn, Context, ExtendedResultOut >;
+}
+
+declare function array<
+Context,
+Block extends ReadonlyArray< any >,
+    ParamsOut = GetArrayBlockParams< Block >,
+    ResultIn = GetArrayBlockResult< Block >,
+    ResultOut = ResultIn,
+> (
+    args: {
+        //  block: DescriptArrayBlockDescription< Block >,
+        block: Block,
+        options?: DescriptBlockOptions<
+            GetArrayBlockParams< Block >,
+            ParamsOut,
+            Context,
+            ResultIn,
+            ResultOut
+        >,
+    },
+): DescriptArrayBlock< GetArrayBlockParams< Block >, Context, ResultOut >;
+
 //  ---------------------------------------------------------------------------------------------------------------  //
+/*
 
 interface DescriptObjectBlockDescription {
     [ property: string ]: DescriptBlock;
@@ -275,25 +384,49 @@ interface DescriptFirstBlock {
 
 //  ---------------------------------------------------------------------------------------------------------------  //
 
-type DescriptBlock =
-    DescriptHttpBlock |
-    DescriptFuncBlock |
-    DescriptArrayBlock |
-    DescriptObjectBlock |
-    DescriptPipeBlock |
-    DescriptFirstBlock;
+*/
+
+type DescriptBlock< Params, Context, Result > =
+    DescriptHttpBlock< Params, Context, Result > |
+    //  DescriptFuncBlock< Params, Context, Result > |
+    DescriptArrayBlock< Params, Context, Result >;
+
+/*
+type DescriptBlock<
+    Params,
+    Result,
+    Context,
+> =
+    DescriptHttpBlock< Params, Result, Context > |
+    DescriptFuncBlock< Params, Result, Context > |
+    DescriptArrayBlock< Params, Result, Context>;
+*/
 
 //  ---------------------------------------------------------------------------------------------------------------  //
 
-declare function run( block: DescriptBlock, args: Partial< DescriptBlockBaseCallbackArgs > ): Promise< DescriptBlockResult >;
+export type GetDescriptBlockResult< T > =
+    T extends DescriptBlock< infer Params, infer Context, infer Result > ? Result : never;
+
+type GetDescriptBlockParams< T > =
+    T extends DescriptBlock< infer Params, infer Context, infer Result > ? Params : never;
+
+//  ---------------------------------------------------------------------------------------------------------------  //
+
+declare function run<
+    Params,
+    Context,
+    Block,
+> (
+    block: Block,
+    args: Partial< DescriptBlockBaseCallbackArgs< Params, Context > >,
+): Promise< GetDescriptBlockResult< Block > >;
+
 declare function is_block( block: any ): boolean;
 
-declare function func( args: { block: DescriptFuncBlockDescription, options?: DescriptBlockOptions } ): DescriptFuncBlock;
-declare function http( args: { block: DescriptHttpBlockDescription, options?: DescriptBlockOptions } ): DescriptHttpBlock;
-declare function array( args: { block: DescriptArrayBlockDescription, options?: DescriptBlockOptions } ): DescriptArrayBlock;
-declare function object( args: { block: DescriptObjectBlockDescription, options?: DescriptBlockOptions } ): DescriptObjectBlock;
-declare function pipe( args: { block: DescriptPipeBlockDescription, options?: DescriptBlockOptions } ): DescriptPipeBlock;
-declare function first( args: { block: DescriptFirstBlockDescription, options?: DescriptBlockOptions } ): DescriptFirstBlock;
+//  declare function array( args: { block: DescriptArrayBlockDescription, options?: DescriptBlockOptions } ): DescriptArrayBlock;
+//  declare function object( args: { block: DescriptObjectBlockDescription, options?: DescriptBlockOptions } ): DescriptObjectBlock;
+//  declare function pipe( args: { block: DescriptPipeBlockDescription, options?: DescriptBlockOptions } ): DescriptPipeBlock;
+//  declare function first( args: { block: DescriptFirstBlockDescription, options?: DescriptBlockOptions } ): DescriptFirstBlock;
 
 declare function error( error: { id: string; message?: string } ): DescriptError;
 declare function is_error( error: any ): boolean;
